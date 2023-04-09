914b220070e6b2411a3c9870e0f1a770dbdcc33a
Nabila <mnabila30163@gmail.com>
2023-04-10 03:02:46 +0600

    commit the feature section

--- src/components/Feature/Feature.jsx
+++ src/components/Feature/Feature.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 const Feature = () => {
     return (
         <div>
-            
+            <div className='text-center mt-28 mb-24'>
+                <h1 className='font-bold text-3xl mb-4'>Featured Jobs</h1>
+                <p className='text-gray-500'>Explore thousands of job opportunities with all the information you need. Its your future</p>
+            </div>
         </div>
     );
 };
