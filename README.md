e0ce9876bf8ff8ea0ae9dc9585699ec395cddc87
Nabila <mnabila30163@gmail.com>
2023-04-12 19:58:10 +0600

    commit the final project

--- README.md
+++ README.md
@@ -6,3 +6,5 @@
 * you can see some job applied form and you can apply the job by clicking a button and these informations are saved in local storage and you can see where you apply
 * there is a pie chart there I added my assignment result
 * there also a blog page. where I add some question and answer.
+
+[Link https://fantastic-druid-d11609.netlify.app/](https://fantastic-druid-d11609.netlify.app/ )
--- src/components/Job/Job.jsx
+++ src/components/Job/Job.jsx
@@ -5,22 +5,30 @@ import JobItems from '../JobItem/JobItems';
 
 
 const Job = () => {
-   
-    
-    const {newArr} = useLoaderData()
+
+
+    const { newArr } = useLoaderData()
 
     // console.log(products)
     return (
         <div>
             <div className='my-10'>
-            <h1 className='text-center  font-bold text-2xl bg-pink-100 p-10 mx-10 rounded-xl'>Job Details</h1>
+                <h1 className='text-center  font-bold text-2xl bg-pink-100 p-10 mx-10 rounded-xl'>Jobs Details</h1>
+
             </div>
+
+            <div className='mb-5 text-center'>
+                <button className='btn btn-secondary mr-5'>Remote</button>
+                <button className='btn btn-secondary mr-5'>Full Time</button>
+            </div>
+
+
             <ul>
                 {
-                    newArr.map(product=> <JobItems
-                        key = {product.id}
+                    newArr.map(product => <JobItems
+                        key={product.id}
                         product={product}
-                    ></JobItems> )
+                    ></JobItems>)
                 }
             </ul>
         </div>
