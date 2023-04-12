aa78221543f295d1f8f5a926c192906e5d88a3f5
Nabila <mnabila30163@gmail.com>
2023-04-12 16:37:33 +0600

    commit the see all button jobs

--- src/components/Feature/Feature.jsx
+++ src/components/Feature/Feature.jsx
@@ -3,12 +3,19 @@ import FeatureShow from '../FeatureShow/FeatureShow';
 
 const Feature = () => {
     const [features, setFeatures] = useState([]);
+    const [showAll, setShowAll] = useState(false);
+
     useEffect(()=>{
         fetch('feature.JSON')
         .then(res => res.json())
         .then(data=> setFeatures(data))
         .catch(error => console.log(error));
-    }, [])
+    }, []);
+
+    const handleSeeAllJobs = () => {
+        setShowAll(true);
+    }
+
     return (
         <div>
             <div className='text-center mt-28 mb-16'>
@@ -16,17 +23,24 @@ const Feature = () => {
                 <p className='text-gray-500'>Explore thousands of job opportunities with all the information you need. Its your future</p>
             </div>
 
-                <div className='grid md:grid-cols-2 md:ml-36 ml-5'>
-                    {
-                        features.map(feature=> <FeatureShow
-                        key = {feature.id}
-                        feature = {feature}
-                        ></FeatureShow> )
-                    }
-                    
+            <div className='grid md:grid-cols-2 md:ml-36 ml-5'>
+                {
+                    features.slice(0, showAll ? features.length : 4).map(feature => (
+                        <FeatureShow
+                            key={feature.id}
+                            feature={feature}
+                        />
+                    ))
+                }
+            </div>
 
-    <center><button className='btn btn-secondary mt-5 md:ml-96 w-32 mb-10'>See All Jobs</button></center>
-                </div>
+            {!showAll && (
+                <center>
+                    <button className='btn btn-secondary mt-5 md:ml-40 w-32 mb-10' onClick={handleSeeAllJobs}>
+                        See All Jobs
+                    </button>
+                </center>
+            )}
         </div>
     );
 };
